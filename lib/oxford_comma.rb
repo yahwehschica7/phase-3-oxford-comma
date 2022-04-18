d5f1db0c07b174f87551cf09d17332a56a478a29
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-18 14:44:06 -0400

    commit tree 2a83cde841652ec0ae41430c7ab69934709d6c1b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    case array.size
+        when 1 
+        array.join
+        when 2
+            array.join(" and ")
+        else
+            array[-1] = "and " + array[-1]
+            array.join(", ")            
+        end
 end
